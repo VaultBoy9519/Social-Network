93a677c4162911bf683bdb6c6a683a348cfa8b46
VaultBoy9519 <illidan9519@mail.ru>
2023-02-28 21:13:04 +0300

    Основная страница разбита на компоненты

---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+const Profile = () => {
+  return <div className="content">
+    <div>
+      <img
+        src="https://st2.depositphotos.com/3257889/9276/i/450/depositphotos_92767826-stock-photo-beautiful-autumn-landscape-in-aosta.jpg" />
+    </div>
+    <div>
+      ava+description
+    </div>
+    <div>
+      My posts
+    </div>
+    <div>
+      New post
+    </div>
+    <div>
+      <div>
+        Post1
+      </div>
+      <div>
+        Post 2
+      </div>
+    </div>
+  </div>;
+};
+
+export default Profile;
